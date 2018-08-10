f3de2042ce6af514e0ea1a7b79a99c2d0b005a97
natalie-io <35521922+natalie-io@users.noreply.github.com>
2018-08-10 14:17:36 +1200

    Add files via upload

---
+++ Shipping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Nat_UdemyCSharpFundamentals
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "EXPRESS";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
